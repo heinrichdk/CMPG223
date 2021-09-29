7c8b322a4e6cb4913d1e41715329e9505a3a5e1a
Heinrich de Klerk <heinrich@skrmiish.com>
2021-09-29 10:27:21 +0200

    added employee page and database service

---
+++ CMPG223/Data/DatabaseService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+
+namespace CMPG223.Data
+{
+    public class DatabaseService
+    {
+        public string DatabaseConnectionString = $"Data Source=DESKTOP-2CM60AH\\SQLEXPRESS;Initial Catalog=CMPG223;Integrated Security=True";
+
+        public async Task<List<Employee>> GetEmployees()
+        {
+            var sql = "Select * from employees";
+            using (var connection = new SqlConnection(DatabaseConnectionString))
+            {
+                var employees = connection.Query<Employee>(sql).ToList();
+
+                return employees;
+            }
+        }
+    }
+}
---
+++ CMPG223/Data/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMPG223.Data
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
